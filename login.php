1f8c4fe846f26bfb910cda1bc20cc051e8e6c7a3
Franko2015 <franko20155@gmail.com>
2023-10-13 12:02:28 -0300

    TODO

--- login.php
+++ login.php
@@ -1,12 +1,31 @@
 <?php
     $email = $_POST["email"];
     $pass = $_POST["pass"];
-    
-    $contenido = "email: $email, contraseña: $pass.\n";
-    $archivo = "datos.txt";
-    
-    if (file_put_contents($archivo, $contenido, FILE_APPEND) == false) {
+
+    // Configuración de la conexión a la base de datos
+    $host = "btblhlgryyu4g4thtksa-mysql.services.clever-cloud.com";
+    $db = "btblhlgryyu4g4thtksa";
+    $user = "u0fqzy3otm6fkgml";
+    $password = "VyKg26R9R2lt0tgVEhNR";
+
+    // Conexión a la base de datos
+    $conn = new mysqli($host, $user, $password, $db);
+
+    // Verificar si la conexión fue exitosa
+    if ($conn->connect_error) {
+        die("Error al conectar a la base de datos: " . $conn->connect_error);
+    }
+
+    // Preparar la consulta SQL
+    $sql = "INSERT INTO tblLogin (usuario, contrasena) VALUES ('$email', '$pass')";
+
+    if ($conn->query($sql) === TRUE) {
         echo "Los datos han sido guardados correctamente.";
     } else {
-        echo "Error al guardar los datos.";
+        echo "Error al guardar los datos: " . $conn->error;
     }
+
+    // Cerrar la conexión
+    $conn->close();
+?>
+
